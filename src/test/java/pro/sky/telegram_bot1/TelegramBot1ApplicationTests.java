00b6eb087c56fd816019bbaaadc83bfea37ce4c7
Pronyushkin_qa <kollaydereva@gmail.com>
2023-02-14 19:18:02 +0300

    Initial commit

---
+++ src/test/java/pro/sky/telegram_bot1/TelegramBot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_bot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
